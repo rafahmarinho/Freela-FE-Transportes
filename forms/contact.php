1a70face0e0f94e30f50e38bb701f975f6133a4f
RafahMarinho <rafael.marinho96@hotmail.com>
2023-03-31 02:54:35 -0300

    Ajustes finais

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,43 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+require '../vendor/PHPMailer-master/src/Exception.php';
+require '../vendor/PHPMailer-master/src/PHPMailer.php';
+require '../vendor/PHPMailer-master/src/SMTP.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Verifica se o formulário foi enviado
+if (isset($_POST['submit'])) {
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    // Define as variáveis do formulário
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    // Configura o objeto PHPMailer
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'seu-email@gmail.com'; // Insira aqui o seu e-mail
+    $mail->Password = 'sua-senha'; // Insira aqui a sua senha
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // Define os destinatários
+    $mail->setFrom($email, $name);
+    $mail->addAddress('comercial@fetransportes.log.br', 'Site F.E. Transportes'); 
+    // Define o assunto e a mensagem
+    $mail->Subject = $subject;
+    $mail->Body = $message;
 
-  echo $contact->send();
+    // Tenta enviar o e-mail
+    try {
+        $mail->send();
+        echo 'Mensagem enviada com sucesso!';
+    } catch (Exception $e) {
+        echo 'Não foi possível enviar a mensagem. Erro: ' . $mail->ErrorInfo;
+    }
+}
 ?>
